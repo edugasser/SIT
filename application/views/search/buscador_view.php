4fca26c7a620cc6409cd332e117f45bd2a5af1cc
edugasser <edugasser@gmail.com>
2012-12-19 10:25:33 +0100

    mejorado buscador

--- application/views/search/buscador_view.php
+++ application/views/search/buscador_view.php
@@ -4,6 +4,6 @@
 if(strlen($searchq)>0){
 if (!empty($posibles)){
 foreach($posibles as $row):?> 
-	<li><a href="<?php echo base_url();?>proyecto/gestion/<?php echo $row->id;?>"><span><?php echo $row->titol;?></span></a></li>
+	<li><a href="<?php echo base_url();?>proyecto/gestion/<?php echo $row->id;?>"><img width="28"src="<?php echo base_url();?>assets/images/bag.png"?><span><?php echo $row->titol;?></span></a></li>
 <?php endforeach;}}?>
 </ul>
