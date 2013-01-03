88c1bfac297ae57c8833bbbcefb771a3186501ca
Vicenç Juan Tomàs Montserrat <vtomasr5@gmail.com>
2013-01-03 16:26:16 +0000

    la cookie expira al tancar el navegador

--- application/config/config.php
+++ application/config/config.php
@@ -248,7 +248,7 @@ $config['encryption_key'] = 'key';
 */
 $config['sess_cookie_name']		= 'ci_session';
 $config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= FALSE;
+$config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'ci_sessions';
@@ -295,7 +295,7 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_cookie_name' = The cookie name
 | 'csrf_expire' = The number in seconds the token should expire.
 */
-$config['csrf_protection'] = FALSE;
+$config['csrf_protection'] = TRUE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
