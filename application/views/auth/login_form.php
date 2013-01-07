d4761b115579271c98398fef85b5fec57a5710cb
edugasser <edugasser@gmail.com>
2013-01-07 16:25:00 +0100

    titulo login

--- application/views/auth/login_form.php
+++ application/views/auth/login_form.php
@@ -4,7 +4,7 @@
 <head>
  <META http-equiv="Content-Type" content="text/html; charset=windows-1252">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>Login Page | Amanda Admin Template</title>
+<title>Igovern 38500</title>
 <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.default.css" type="text/css" />
 <script type="text/javascript" src="<?php echo base_url();?>assets/js/plugins/jquery-1.7.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url();?>assets/js/plugins/jquery-ui-1.8.16.custom.min.js"></script>
